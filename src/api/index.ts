08f91a9b9b5c1f941406ac3d39a31c905d824a87
iceylia <iceylia@outlook.com>
2025-01-30 13:50:35 +0800

    change static url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
-let staticUrl = "https://static.iceylia.top/static/"
-// let staticUrl = "http://47.109.54.76/static/"
+// let staticUrl = "https://static.iceylia.top/static/"
+let staticUrl = "http://47.109.54.76/static/"
 export { staticUrl }
