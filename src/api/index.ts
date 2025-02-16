7bb39c71bbd6f28d2c893de20f9d4958c1d9f573
iceylia <iceylia@outlook.com>
2025-02-16 19:49:58 +0800

    update env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,4 @@
-let staticUrl = "https://static.iceylia.xyz/static/"
 
-// let socketUrl = "https://static.iceylia.xyz:3000";
-// let socketUrl = "http://52.41.36.82:3000";
-let socketUrl = "http://127.0.0.1:3000";
-
-// let staticUrl = "http://47.109.54.76/static/"
+let staticUrl = import.meta.env.STATIC_URL;
+let socketUrl = import.meta.env.SOCKET_URL;
 export { staticUrl, socketUrl }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+BACKEND_URL="http://47.109.54.76:3000"
